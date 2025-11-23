910a6487877f3d00d48eb7fea927a48107bd2b18
liamjackson34-art <liamjackson34@gmail.com>
2025-11-23 23:38:43 +0000

    add_friend.sh

--- BashBook/add_friend.sh
+++ BashBook/add_friend.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [ $# -ne 2 ];then
+if [ $# -ne 2 ];then # Check if the number of arguments passed is exactly 2
 echo "nok: bad request"
 exit 1
 fi
-id="$1"
+if [ ! -d "$id" ]; then # Assign first argument to variable id and second to frien
+id="$1" 
 friend="$2"
-
-if [ ! -d "$id" ]; then
+# Check if a directory named after the id exists
 echo "nok: user'$user' does not exist"
 exit 1
 fi
@@ -14,6 +14,6 @@ if grep -qx "$friend" "$id/friends.txt" 2>/dev/null; then
 echo "ok: friend added!"
 exit 0
 fi
-echo "$friend" >> "$id/friends.txt"
+echo "$friend" >> "$id/friends.txt" # Append friend to the friends.txt file for the use
 echo "ok:friend added!"
 
