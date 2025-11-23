1215d6cc85349632b3d219521624c0e896b2c1c3
liamjackson34-art <liamjackson34@gmail.com>
2025-11-23 23:43:31 +0000

    server.sh

--- BashBook/server.sh
+++ BashBook/server.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
-while true; do #loop to keep server going
-echo -n"> " #user prompt for input in terminal
-read -r cmd args #reads command from user and its arguments
-set -- $cmd $args
-request=$1
-shift
-case "$request" in
-create)
-./create.sh "$@";;
-add)
-./add_friend.sh "$@";;
-post)
-./post_message.sh "$@";;
-display)
-./display_wall.sh "$@";;
-*)
-echo "nok: bad request";;
-esac
+# Infinite loop to keep the server running and accepting commands continuously
+while true; do 
+  # Display a prompt symbol '>' without a newline for user input
+  echo -n "> "
+  
+  # Read user input into 'cmd' (command) and 'args' (arguments)
+  read -r cmd args
+  
+  # Set positional parameters from command and arguments for easy forwarding
+  set -- $cmd $args
+  
+  # Assign the first positional parameter to 'request' and shift it from the list
+  request=$1
+  shift
+  
+  # Handle the command based on the 'request' value
+  case "$request" in
+    # Executes the create.sh script
+    create)
+      ./create.sh "$@"
+      ;;
+    
+    # Executes the add_friend.sh script
+    add)
+      ./add_friend.sh "$@"
+      ;;
+    
+    # Executes the post_message.sh script
+    post)
+      ./post_message.sh "$@"
+      ;;
+    
+    # Executes the display_wall.sh script 
+    display)
+      ./display_wall.sh "$@"
+      ;;
+    
+    # returnx an error message
+    *)
+      echo "nok: bad request"
+      ;;
+  esac
 done
