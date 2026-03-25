30a2e4ae49ead65decf65577af3497a9823af6d0
liamjackson34-art <liamjackson34@gmail.com>
2026-03-25 23:41:38 +0000

    Delete BashBook directory

--- BashBook/acquire.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-lock="lock_$1"
-
-# spin until we can create the lock directory
-while ! mkdir "$lock" 2>/dev/null; do
-  sleep 0.1
-done
--- BashBook/release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-lock="lock_$1"
-rmdir "$lock" 2>/dev/null
